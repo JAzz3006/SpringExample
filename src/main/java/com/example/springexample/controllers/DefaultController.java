592e0916e2cae4e9ca19a47e3e60e86fdb23dca8
Grigoriy Chernykh <che90@mail.ru>
2025-10-08 16:54:51 +0500

    todo list in progress

--- src/main/java/com/example/springexample/controllers/DefaultController.java
+++ src/main/java/com/example/springexample/controllers/DefaultController.java
@@ -1,16 +1,38 @@
 package com.example.springexample.controllers;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
 public class DefaultController {
 
-    @ResponseBody
-    //@RequestMapping(path="/hello")
-    public String defaultController(@RequestParam String name){
-        return "Hello, Spring! Param value: " + name;
+    private Map<Integer, String> todo;
+
+    public DefaultController(){
+        todo = new TreeMap<>();
+        todo.put(1, "one");
+        todo.put(2, "two");
+    }
+
+    @GetMapping(path="/todo")
+    public Map<Integer, String> getAllList (){
+        return todo;
+    }
+
+    @PostMapping(path = "/todo")
+    public ResponseEntity addOne (@RequestParam String item){
+        if (item == null || item.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        int newKey = todo.isEmpty() ? 1 : Collections.max(todo.keySet()) + 1;
+        todo.put(newKey, item);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
